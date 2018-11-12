85e79713cd855dc2d7d4bd26d459bc9f1f4e23da
Peter Kurfer <peter.kurfer@fh-rosenheim.de>
2018-11-12 01:16:47 +0100

    Setup basic JavaFX project structure

---
+++ src/main/java/de/thro/inf/prg3/a07/controllers/MainController.java
@@ -0,0 +1,15 @@
+package de.thro.inf.prg3.a07.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+	@Override
+	public void initialize(URL url, ResourceBundle resourceBundle) {
+		// logic to initialize the view goes here
+	}
+
+}
