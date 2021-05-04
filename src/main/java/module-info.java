b6565aa912ab62461c6a4e2a3d145a72645792db
Korbinian <korbinian@remeeting.com>
2021-05-04 09:37:23 +0200

    pinning okhttp/retrofit/jfx and gradle

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,18 @@
 module ohm.softa.a07 {
 	requires javafx.controls;
 	requires javafx.fxml;
+
 	requires org.apache.commons.lang3;
+	requires org.apache.logging.log4j;
+	requires retrofit2;
+	requires retrofit2.converter.gson;
+	requires okhttp3;
+	requires okhttp3.logging;
 
 	opens ohm.softa.a07 to javafx.fxml;
 	opens ohm.softa.a07.controllers to javafx.fxml;
+	opens ohm.softa.a07.model to gson;
 
 	exports ohm.softa.a07 to javafx.graphics;
+	exports ohm.softa.a07.model;
 }
